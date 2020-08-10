a1d318fe62ba942e5d196cd0379fb6512bb57b3f
tiki7732 <lsaidah@gmail.com>
2020-08-10 15:34:42 -0500

    added phase 1 of largest sub

--- algorithms.rb
+++ algorithms.rb
@@ -7,6 +7,18 @@ def my_min(array)
 end 
 #O(n^2)
 
+def largest_contiguous_subsum(array)
+    sub_arrs = []
+    array.each_index do |ind1|
+        array[ind1..-1].each_index do |ind2|
+            sub_arrs.push(array[ind1..ind2+ind1])
+        end
+    end
+    max_sum = sub_arrs.first.sum
+    sub_arrs.each {|sub| max_sum = sub.sum if sub.sum >= max_sum }
+    return max_sum
+end
+
 #Phase2
 def my_min2(array)
     min = array.first
@@ -15,5 +27,13 @@ def my_min2(array)
 end
 #O(n)
 
+
+
 p my_min([4,3,2,6,-5,5,8,10])
 p my_min2([4,3,2,6,-5,5,8,10])
+list = [5, 3, -7]
+list2 = [2, 3, -6, 7, -6, 7]
+list3 = [-5, -1, -3]
+p largest_contiguous_subsum(list)
+p largest_contiguous_subsum(list2)
+p largest_contiguous_subsum(list3)
